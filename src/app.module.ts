4233f9b133d47f0cb03862b89cd490aa89fff46e
Cristian Sarmiento <cristian.sarmiento@icloud.com>
2023-02-04 18:10:10 -0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { UsersController } from './users/users.controller';
-import { SearchController } from './search/search.controller';
-import { SuggestionsController } from './suggestions/suggestions.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, UsersController, SearchController, SuggestionsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
